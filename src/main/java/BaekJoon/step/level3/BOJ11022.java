c739506293c3640a519d11f0ce58b0f929e6c764
ShinKangWon <ekdrms5153@naver.com>
2023-09-20 11:38:58 +0900

    11022 clear

--- src/main/java/BaekJoon/step/level3/BOJ11022.java
+++ src/main/java/BaekJoon/step/level3/BOJ11022.java
@@ -2,22 +2,21 @@ package BaekJoon.step.level3;
 
 import java.io.*;
 
-public class BOJ11021 {
+public class BOJ11022 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         int testCaseQuantity = Integer.parseInt(reader.readLine());
-
         for (int i = 0; i < testCaseQuantity; i++) {
             String testCase = reader.readLine();
-            String[] testCaseSplit = testCase.split(" ");
+            String[] testCaseArray = testCase.split(" ");
 
-            int a = Integer.parseInt(testCaseSplit[0]);
-            int b = Integer.parseInt(testCaseSplit[1]);
+            int a = Integer.parseInt(testCaseArray[0]);
+            int b = Integer.parseInt(testCaseArray[1]);
 
-            writer.write("Case #" + (i + 1) + ": " + (a + b) + "\n");
-            writer.flush();
+            writer.write("Case #" + (i + 1) + ": " + a + " + " + b + " = " + (a + b)+ "\n");
         }
+        writer.flush();
         reader.close();
         writer.close();
     }
