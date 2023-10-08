68bb5696905f608d839813562520ad33f6c5cf7b
ShinKangWon <ekdrms5153@naver.com>
2023-10-08 23:37:30 +0900

    11654
    solved

---
+++ src/main/java/BaekJoon/step/level5/BOJ11654.java
@@ -0,0 +1,15 @@
+package BaekJoon.step.level5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        char a = reader.readLine().charAt(0);
+        int asc = a;
+        System.out.println(asc);
+    }
+}
